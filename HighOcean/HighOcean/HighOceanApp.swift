8058110a63d00502efdd9edbc0976f04a59de4ee
HansolWorld <hs9269@gmail.com>
2023-05-03 11:21:42 +0900

    create project

---
+++ HighOcean/HighOcean/HighOceanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighOceanApp.swift
+//  HighOcean
+//
+//  Created by apple on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct HighOceanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
