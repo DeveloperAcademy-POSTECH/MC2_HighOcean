956698f4ebf149a0cdc0f553297c0695cefe56ca
tosuheon@gmail.com <tosuheon@gmail.com>
2023-05-04 02:26:37 +0900

    Profile view 2st

--- HighOcean/HighOcean/ProfileView.swift
+++ HighOcean/HighOcean/ProfileView.swift
@@ -6,6 +6,13 @@
 //
 
 import SwiftUI
+struct ColorManager {
+    static let BackgroundColor = Color("BackcolorA")
+    static let PrimaryColor = Color("PrimaryA")
+    
+    static let TitelColor = Color("TitelColorA")
+}
+
 
 struct ProfileView: View {
     @State var name: String = ""
@@ -13,28 +20,46 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             
-                    VStack {
-                        Text("가입을 축하드려요")
-                            .font(.system(size: 28))
-                            .fontWeight(.bold)
-                        Text("이름을 입력해주세요!")
-                            .font(.system(size: 28))
-                            .fontWeight(.bold)
-                        
-                        
-                        
-                        TextField("Name Input", text: $name)
-                            .background(Color.white)
-                            .textFieldStyle(.roundedBorder)
+            ZStack(){
+                ColorManager.BackgroundColor.ignoresSafeArea()
+                VStack(alignment: .leading) {
                     
+                    Text("반가워요!")
+                        .font(.system(size: 28))
+                        .fontWeight(.bold)
                         
-                        Button("Next"){
+                        .offset(y: 110)
+                    Text("이름을 입력해주세요")
+                        .font(.system(size: 28))
+                        .fontWeight(.bold)
+                        .offset(y: 110)
+                    
+                    VStack(){
+                        TextField("이름", text: $name)
+                            .frame(width: 338.0, height:62.0)
+                            .padding(.leading,16)
+                            .background(RoundedRectangle(cornerRadius: 10).fill(Color.white))
                             
-                        }
+                            .offset(y: 300)
                     }
+                    Spacer()
+                      
+                    
                 
-  }
- }
+                    Button("다음"){}
+                        .padding(.horizontal)
+                        .frame(width: 354, height:54)
+                        .background(RoundedRectangle(cornerRadius: 10).fill(ColorManager.PrimaryColor))
+                    
+                    
+                }
+                .padding(20)
+                
+            }
+                
+        }
+    }
+    
 }
 
 // 미리보기
