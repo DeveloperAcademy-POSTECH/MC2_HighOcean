af6097507c35ee1b9389511d08099d815977fb8f
tosuheon@gmail.com <tosuheon@gmail.com>
2023-05-08 06:56:49 +0900

    2305080656

--- HighOcean/HighOcean/CollectionView.swift
+++ HighOcean/HighOcean/CollectionView.swift
@@ -11,39 +11,50 @@ struct CollectionView: View {
     
     let data = Array(1...1000).map { "목록 \($0)"}
     let columns = [
-            GridItem(.adaptive(minimum: 100))
+//            GridItem(.adaptive(minimum: 100))
+        GridItem(.flexible(maximum: 110)),
+        GridItem(.flexible(maximum: 110)),
+        GridItem(.flexible(maximum: 110)),
         ]
     
+    @State private var cardFormA = false
+    @State private var cardFormB = false
+    @State private var cardFormC = false
     var body: some View {
     
-        LazyVGrid(columns: columns, spacing: 2) {
+        LazyVGrid(columns: columns, spacing: 26) {
             
             VStack{
-                Color("Green")
-                Text("날짜 / 요일")
+                if cardFormA {
+                    Image(systemName: "pencil")
+                }
+                Image(systemName: "moon.stars.fill")
+                Text("날짜 / 시간").onTapGesture {
+                    self.cardFormA.toggle()
+                }
             }
             VStack{
-                Color("Green")
-                Text("날짜 / 요일")
+                if cardFormB {
+                    Image(systemName: "pencil")
+                }
+                Image(systemName: "moon.stars.fill")
+                Text("날짜 / 시간").onTapGesture {
+                    self.cardFormB.toggle()
+                }
             }
             VStack{
-                Color("Green")
-                Text("날짜 / 요일")
+                if cardFormC {
+                    Image(systemName: "pencil")
+                }
+                Image(systemName: "moon.stars.fill")
+                Text("날짜 / 시간").onTapGesture {
+                    self.cardFormC.toggle()
+                }
             }
-            VStack{
-                Color("Green")
-                Text("날짜 / 요일")
-            }
-            VStack{
-                Color("Green")
-                Text("날짜 / 요일")
-            }
-            VStack{
-                Color("Green")
-                Text("날짜 / 요일")
-            }
+            
 
         }
+        
 
     }
 }
