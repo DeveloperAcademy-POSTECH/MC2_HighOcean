3d37b894e7b845da151c7d2a790ce0255d49343b
aewols <i0y0j057@gmail.com>
2023-05-10 14:24:51 +0900

    - ThumbnailCardView를 추가해 Grid 내 Card 이미지 조절하여 LikedCardCollectionView에 연결 - ThumbnailCardView 수정

--- HighOcean/HighOcean/LikedCard/ThumbnailCardView.swift
+++ HighOcean/HighOcean/LikedCard/ThumbnailCardView.swift
@@ -11,12 +11,9 @@ struct ThumbnailCardView: View {
     @Binding var degree : Double
     @Binding var card: Card
     
-    @State private var image = UIImage()
-    @State private var createdDate = Date()
-    
     var body: some View {
         ZStack{
-            RoundedRectangle(cornerRadius: 11)
+            RoundedRectangle(cornerRadius: 20)
                 .fill(.white)
                 .shadow(radius: 3)
                 .frame(width: 144, height: 235)
@@ -26,9 +23,9 @@ struct ThumbnailCardView: View {
             LinearGradient(
                 gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))]),
                 startPoint: .top, endPoint: .bottom)
-            .frame(width: 124, height: 213)
-            .opacity(0.3)
-            .blendMode(.multiply)
+                .frame(width: 124, height: 213)
+                .opacity(0.3)
+                .blendMode(.multiply)
             DateAndDay
         }
         .rotation3DEffect(Angle(degrees: degree), axis: (x: 0, y: 1, z: 0))
@@ -37,7 +34,7 @@ struct ThumbnailCardView: View {
     var DateAndDay: some View {
         VStack {
             Spacer()
-            Text("24")
+            Text(remakedDate())
                 .font(.title2)
                 .fontWeight(.semibold)
                 .foregroundColor(Color("Secondary"))
@@ -47,7 +44,7 @@ struct ThumbnailCardView: View {
                 .frame(width: 50, height: 2)
                 .background(Color("Secondary"))
                 .shadow(radius: 2)
-            Text("월요일")
+            Text(remakedDay())
                 .fontWeight(.medium)
                 .foregroundColor(Color("Secondary"))
                 .shadow(radius: 2)
@@ -55,7 +52,35 @@ struct ThumbnailCardView: View {
         .padding(.bottom, 30)
         .frame(width: 124, height: 213)
     }
-
     
+    func remakedDate() -> String {
+        let dateString = card.createdDate
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy-MM-dd"
+        guard let date = formatter.date(from: dateString) else {
+            return ""
+        }
+        formatter.dateFormat = "dd"
+        return formatter.string(from: date)
+    }
+    
+    func remakedDay() -> String {
+        let dateString = card.createdDate
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy-MM-dd"
+        guard let date = formatter.date(from: dateString) else {
+            return ""
+        }
+        formatter.locale = Locale(identifier: "ko")
+        formatter.dateFormat = "E요일"
+        return formatter.string(from: date)
+    }
+}
+
+struct ThumbnailCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThumbnailCardView(degree: .constant(0), card: .constant(Cards().cards[0]))
+            .environmentObject(Cards())
+    }
 }
 
