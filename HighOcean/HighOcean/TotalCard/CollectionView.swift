aec213026545962e528a2876fb84f0f1b48cb6ea
aewols <i0y0j057@gmail.com>
2023-05-10 14:40:55 +0900

    - TotalThumbnailCardView 생성하여 TotalCardCollectionView에서 사용될 Card 사이즈 조절 - CollectionView에서 Sort한 데이터 연결 시도

--- HighOcean/HighOcean/TotalCard/CollectionView.swift
+++ HighOcean/HighOcean/TotalCard/CollectionView.swift
@@ -9,46 +9,28 @@ import SwiftUI
 
 struct CollectionView: View {
     
+    @State private var selectedCard: Card?
+        @EnvironmentObject var cards: Cards
+    
     let data = Array(1...1000).map { "목록 \($0)"}
     let columns = [
-//            GridItem(.adaptive(minimum: 100))
         GridItem(.flexible(maximum: 110)),
         GridItem(.flexible(maximum: 110)),
         GridItem(.flexible(maximum: 110)),
         ]
     
-    @State private var cardFormA = false
-    @State private var cardFormB = false
-    @State private var cardFormC = false
     var body: some View {
     
         LazyVGrid(columns: columns, spacing: 26) {
             
-            VStack{
-                if cardFormA {
-                    Image(systemName: "pencil")
-                }
-                Image(systemName: "moon.stars.fill")
-                Text("날짜 / 시간").onTapGesture {
-                    self.cardFormA.toggle()
-                }
-            }
-            VStack{
-                if cardFormB {
-                    Image(systemName: "pencil")
-                }
-                Image(systemName: "moon.stars.fill")
-                Text("날짜 / 시간").onTapGesture {
-                    self.cardFormB.toggle()
-                }
-            }
-            VStack{
-                if cardFormC {
-                    Image(systemName: "pencil")
-                }
-                Image(systemName: "moon.stars.fill")
-                Text("날짜 / 시간").onTapGesture {
-                    self.cardFormC.toggle()
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 144))], spacing: 16) {
+                ForEach($cards.likedCards) { $card in
+                    Button {
+                        self.selectedCard = card
+                    } label: {
+                        TotalThumbnailCardView(degree: .constant(0), card: $card)
+                    }
+                    .buttonStyle(PlainButtonStyle())
                 }
             }
             
