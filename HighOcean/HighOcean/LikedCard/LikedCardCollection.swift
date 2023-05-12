7a64992f531829a4300fa088cbf5fbab2781b625
aewols <i0y0j057@gmail.com>
2023-05-13 04:55:04 +0900

    - LikedCardCollection 수정

--- HighOcean/HighOcean/LikedCard/LikedCardCollection.swift
+++ HighOcean/HighOcean/LikedCard/LikedCardCollection.swift
@@ -3,7 +3,7 @@ import SwiftUI
 
 struct LikedCardCollection: View {
 
-    @State private var selectedCard: Card?
+    @State private var selectedCard: Binding<Card?> = .constant(nil)
     @State private var showModal = false
     @EnvironmentObject var cards: Cards
     
@@ -34,7 +34,13 @@ struct LikedCardCollection: View {
                     LazyVGrid(columns: columns, spacing: 20){
                         ForEach(pageItems, id: \.self) { card in
                             Button {
-                                self.selectedCard = pageItems[index]
+                                self.selectedCard = Binding(get: {
+                                    card
+                                } , set: { newValue in
+                                    if let index = cards.likedCards.firstIndex(of: card) {
+                                        cards.likedCards[index] = newValue!
+                                    }
+                                })
                                 self.showModal = true
                             } label: {
                                 ThumbnailCardView(degree: .constant(0), card: .constant(card))
@@ -45,37 +51,35 @@ struct LikedCardCollection: View {
                 }
             }
             .tabViewStyle(PageTabViewStyle())
-            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-            .offset(y: 16)
-            
-            
-            if self.showModal {
-                ZStack{
-                    if let selectedCardIndex = likedCards.firstIndex(of: selectedCard!) {
-                        CardView(card: $cards.likedCards[selectedCardIndex])
-                    }
-                    
-                    VStack{
-                        Spacer()
+           .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+           .offset(y: 16)
+               if self.showModal {
+                   ZStack{
+                       if let selectedCard = selectedCard.wrappedValue {
+                           CardView(card: self.$cards.likedCards[self.cards.likedCards.firstIndex(of: selectedCard)!])
+                       }
+                       
+                       VStack{
+                           Spacer()
 
-                        Button(action: {
-                            withAnimation{
-                                self.showModal.toggle()
-                            }
-                        }, label: {
-                            Image(systemName: "xmark").resizable()
-                                .frame(width: 15, height: 15)
-                                .foregroundColor(.black)
-                                .padding(20)
-                                .background(Color.white)
-                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                                .padding(.bottom, 80)
-                        })
-                    }
-                }
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                .background(Color.black.opacity(0.5))
-                .edgesIgnoringSafeArea(.all)
+                           Button(action: {
+                               withAnimation{
+                                   self.showModal.toggle()
+                               }
+                           }, label: {
+                               Image(systemName: "xmark").resizable()
+                                   .frame(width: 15, height: 15)
+                                   .foregroundColor(.black)
+                                   .padding(20)
+                                   .background(Color.white)
+                                   .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                   .padding(.bottom, 80)
+                           })
+                       }
+                   }
+                   .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+                   .background(Color.black.opacity(0.5))
+                    .edgesIgnoringSafeArea(.all)
             }
         }
         .navigationTitle("저장한 카드")
